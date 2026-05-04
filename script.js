5eff0bd1a34c513ad8fc40422521d616c294f8d0
kshadrick <kshadrick@cpp.edu>
2026-05-03 20:10:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -184,6 +184,7 @@ let userExams = [
 ];
 
 function addClassExam() {
+    console.log("BUTTON CLICKED");
     const className = document.getElementById("class-name").value.trim();
     const examName = document.getElementById("exam-name").value.trim();
     const date = document.getElementById("exam-date").value;
